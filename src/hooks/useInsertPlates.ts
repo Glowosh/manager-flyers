9f4956d60648bcfa3c37320cff13e43ec4db6f23
Yago valuche <valuchedevs@gmail.com>
2024-06-03 10:29:58 -0300

    fix: Adjusting how we replace dates

--- src/hooks/useInsertPlates.ts
+++ src/hooks/useInsertPlates.ts
@@ -6,6 +6,27 @@ const dateDiffInDays = (date1: Date, date2: Date) => {
   return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 };
 
+function getCurrentTimestampWithTimezone() {
+  const now = new Date();
+
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, "0");
+  const day = String(now.getDate()).padStart(2, "0");
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const seconds = String(now.getSeconds()).padStart(2, "0");
+
+  const timezoneOffset = -now.getTimezoneOffset();
+  const sign = timezoneOffset >= 0 ? "+" : "-";
+  const offsetHours = String(
+    Math.floor(Math.abs(timezoneOffset) / 60)
+  ).padStart(2, "0");
+  const offsetMinutes = String(Math.abs(timezoneOffset) % 60).padStart(2, "0");
+
+  const formattedTimestamp = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}${sign}${offsetHours}:${offsetMinutes}`;
+  return formattedTimestamp;
+}
+
 export const useInsertPlates = () => {
   const [isLoadingPlate, setIsLoadingPlate] = useState(false);
   const [error, setError] = useState<string | null>(null);
@@ -29,25 +50,40 @@ export const useInsertPlates = () => {
       if (fetchError) {
         throw fetchError;
       }
+      const lastPlate = existingPlates?.[0];
 
       if (existingPlates.length > 0) {
-        const lastPlate = existingPlates[0];
         const lastUpdateDate = new Date(lastPlate.last_update);
         const currentDate = new Date();
 
         const daysDifference = dateDiffInDays(lastUpdateDate, currentDate);
 
-        if (daysDifference < 7) {
+        if (daysDifference < 20) {
           return {
             success: false,
-            text: "Plate number cannot be added. It's been less than 7 days since the last update.",
+            text: "Plate number cannot be added. It's been less than 20 days since the last update.",
           };
         }
       }
 
-      const { error: insertError } = await supabase
-        .from("list_flyers")
-        .insert([{ plate_number: newPlateNumber }]);
+      const { error: insertError } = lastPlate
+        ? await supabase
+            .from("list_flyers")
+            .update([
+              {
+                plate_number: newPlateNumber,
+                last_update: getCurrentTimestampWithTimezone(),
+              },
+            ])
+            .match({
+              id: lastPlate?.id,
+            })
+        : await supabase.from("list_flyers").insert([
+            {
+              plate_number: newPlateNumber,
+              lastPlate: getCurrentTimestampWithTimezone(),
+            },
+          ]);
 
       if (insertError) {
         throw insertError;
